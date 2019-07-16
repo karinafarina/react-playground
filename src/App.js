d4e73f1972c694b5f8fb4c2bafbc534d0fbe0ee1
Karina Gaulin <karina@domandtom.com>
2019-07-16 15:37:50 -0400

    Component Composition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <Split className='left' flexBasis={2}>This is the content for left Lorem Ipsum</Split>
+      <Split className="right">This is content for the right Lorem</Split>
+    </main>
   );
 }
 
